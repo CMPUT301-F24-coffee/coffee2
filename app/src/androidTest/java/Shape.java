119f8ff18484688f06ff80956a32812dabce8dc6
Liqi Yin <115316139+Yinliqi@users.noreply.github.com>
2024-09-27 10:59:52 -0600

    Update Shape.java

--- app/src/androidTest/java/Shape.java
+++ app/src/androidTest/java/Shape.java
@@ -2,4 +2,24 @@ public class Shape {
     private int x;
     private int y;
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    public Shape (Integer x, Integer y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
 }
