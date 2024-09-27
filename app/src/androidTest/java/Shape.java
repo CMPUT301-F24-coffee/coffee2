9949925ee0c685e1ad76c78bc2cc46b3b3cf5df6
Sridhar Saravanakumar <104048833+sp00ds@users.noreply.github.com>
2024-09-27 14:32:09 -0600

    Update Shape.java

--- app/src/androidTest/java/Shape.java
+++ app/src/androidTest/java/Shape.java
@@ -2,7 +2,7 @@ public class Shape {
     private int x;
     private int y;
 
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "purple";
     public Shape (Integer x, Integer y){
         this.x = x;
         this.y = y;
